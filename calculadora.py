742c59254a5c3049a850912e9fa6fa9a1c2810ce
Carlos Raul Martinez Herrera <240I0011@martineztorre.tecnm.mx>
2024-09-19 15:35:59 -0600

    Add files via upload
    Signed-off-by: Carlos Raul Martinez Herrera <240I0011@martineztorre.tecnm.mx>

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+a=float (input("igrese un numero: "))
+b=float(input("ingrese un numero: "))
+suma = (a+b)
+print("el resutado es : ", str(suma))
+if suma>0:
+    print("el resultado positivo")
+    print("gracias por utilizar la calculadora del equipo 5")
+
+else:
+    print("el resultado es negativo ")
+    print("gracias por utilizar la calculadora del equipo 5" )
+
+print(a-b)
+print(a*b)
+print(a/b)
+
+    
